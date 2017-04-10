bb3d57b03ba50f46d0e522aca60bbd58da811b82
root <root@overcloud-controller-1.localdomain>
2017-04-10 19:35:26 +0000

    added lbaas to api_extensions in tempest.conf

--- lbaasv2/install-lbaasv2-tests.sh
+++ lbaasv2/install-lbaasv2-tests.sh
@@ -1,6 +1,11 @@
-mkdir /tmp/myneutron_lbaas_tests
-cp -r  /usr/lib/python2.7/site-packages/neutron_lbaas/ /tmp/myneutron_lbaas_tests/neutron_lbaas/
-cp ~/mytempest/etc/tempest.conf /tmp/myneutron_lbaas_tests/
-cp run_lbaasv2_tests.sh  /tmp/myneutron_lbaas_tests/
-cp .testr.conf /tmp/myneutron_lbaas_tests/
+mkdir -p /tmp/myneutron_lbaas_tests
+cp -rf /usr/lib/python2.7/site-packages/neutron_lbaas/ /tmp/myneutron_lbaas_tests/neutron_lbaas/
+
+#lbaas needs to be in the api_extensions (as well as lbaasv2 which should already be ther)
+cp -f ~/mytempest/etc/tempest.conf /tmp/myneutron_lbaas_tests/
+api_extensions=$(crudini --get /tmp/myneutron_lbaas_tests/tempest.conf network-feature-enabled api_extensions)
+crudini --set /tmp/myneutron_lbaas_tests/tempest.conf network-feature-enabled api_extensions $api_extensions,lbaas
+
+cp -f run_lbaasv2_tests.sh  /tmp/myneutron_lbaas_tests/
+cp -f .testr.conf /tmp/myneutron_lbaas_tests/
 chmod +x  /tmp/myneutron_lbaas_tests/run_lbaasv2_tests.sh
